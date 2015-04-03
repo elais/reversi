0e039d0bb0aee7fe8548257b71992eb051cd685e
Elais Player <elais.player@gmail.com>
2015-04-02 22:11:30 -0500

    need to add comments

--- src/main/java/edu/uab/cis/reversi/strategy/group2/Group2Strategy.java
+++ src/main/java/edu/uab/cis/reversi/strategy/group2/Group2Strategy.java
@@ -9,14 +9,17 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 import java.util.Set;
-import java.util.function.UnaryOperator;
+import java.util.function.ToDoubleFunction;
 import edu.uab.cis.reversi.Board;
 import java.util.Comparator;
 import edu.uab.cis.reversi.Move;
+import edu.uab.cis.reversi.strategy.group2.*;
 import edu.uab.cis.reversi.Player;
 import java.util.PriorityQueue;
 import edu.uab.cis.reversi.Square;
 import edu.uab.cis.reversi.Strategy;
+import java.util.AbstractMap;
+import java.util.Collections;
 import java.util.HashSet;
 import java.util.Iterator;
 import java.util.Map;
@@ -24,174 +27,120 @@ import java.util.Queue;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.TimeUnit;
 import java.util.function.Function;
+import java.util.function.ToDoubleFunction;
+
 /**
  *
  * @author elais
  */
 class FunctionalUtils {
-	static final UnaryOperator<Node> coin_parity = (final Node node) -> {
-          int coin_parity = 100 * ((node.getBoard().getPlayerSquareCounts().get(node.getPlayer()) -
-              node.getBoard().getPlayerSquareCounts().get(node.getOpponent()))/
-              (node.getBoard().getPlayerSquareCounts().get(node.getPlayer())+
-              node.getBoard().getPlayerSquareCounts().get(node.getOpponent())));
-              node.setF(coin_parity);		
-          return node;
-	};
-        
-        static final UnaryOperator<Node> start_here = (final Node node) -> {
-          int yours = 0;
-          return node;
-        };
+
+  static final ToDoubleFunction<Node> coin_parity = (final Node node) -> {
+    double coin_parity;
+    coin_parity = 100 * ((node.getBoard().getPlayerSquareCounts().get(node.getPlayer()).doubleValue()
+            - node.getBoard().getPlayerSquareCounts().get(node.getOpponent()).doubleValue())
+            / (node.getBoard().getPlayerSquareCounts().get(node.getPlayer()).doubleValue()
+            + node.getBoard().getPlayerSquareCounts().get(node.getOpponent()).doubleValue()));
+    //System.out.println(coin_parity);
+    return coin_parity;
+  };
+
+  static final ToDoubleFunction<Node> start_here = (final Node node) -> {
+    double eval = 0;
+    return eval;
+  };
 }
 
 class Evaluator {
-       /** a variable referencing a lambda taking two Integer arguments and returning an Integer: */
-	private final UnaryOperator<Node> strategy;
- 
-	public Evaluator(final UnaryOperator<Node> lambda) {
-                strategy = lambda;
-        }
- 
-        public Node executeStrategy(final Node a) {
-                return strategy.apply(a);
-        }
+
+  /**
+   * a variable referencing a lambda taking two Integer arguments and returning
+   * an Integer:
+   */
+  private final ToDoubleFunction<Node> strategy;
+
+  public Evaluator(final ToDoubleFunction<Node> lambda) {
+    strategy = lambda;
+  }
+
+  public Double exec(final Node a) {
+    return strategy.applyAsDouble(a);
+  }
 }
 
+public class Group2Strategy implements Strategy {
 
-public class Group2Strategy implements Strategy{
-    public static Node alphabeta(Node node, int depth, int alpha, int beta, Evaluator evaluate){
-      Node parent = node;
-      List<Node> child_list = new ArrayList();
-      if(depth==0)
-        return evaluate.executeStrategy(node);
+  private Tuple alphabeta(Node node, int depth, double alpha, double beta, Evaluator evaluate) {
+    
+    Node best = node; // placeholder for highest scoring child node
+    double bestResult = Double.NEGATIVE_INFINITY; //initialize alpha
+    
+    List<Node> child_list = new ArrayList();
+    if (depth == 0) {
       
-      if(parent.getBoard().getCurrentPossibleSquares().isEmpty()){
-        Node n = new Node(parent.getBoard().pass());
+      Tuple t = new Tuple(node, evaluate.exec(node));
+      return t;
+    }
+
+    if (node.getBoard().getCurrentPossibleSquares().isEmpty()) {
+      Node n = new Node(node.getBoard().pass());
+      n.setSquare(Square.PASS);
+      child_list.add(n);
+    } else{
+      Iterator<Square> it = node.getBoard().getCurrentPossibleSquares().iterator();
+      while (it.hasNext()) {
+        Square s = it.next();
+        Node n = new Node(node.getBoard().play(s));
+        n.setSquare(s);
         child_list.add(n);
-      } else{
-        Iterator<Square> it = parent.getBoard().getCurrentPossibleSquares().iterator();
-        while(it.hasNext()){
-          Square s = it.next();
-          Node n = new Node(parent.getBoard().play(s));
-          n.setSquare(s);
-          child_list.add(n);
-        }
       }
-      Iterator<Node> children = child_list.iterator();
-      while(children.hasNext()){
-        Node child = children.next();
-        Node alpha_child = alphabeta(child, depth - 1, -beta, -alpha, evaluate);
-        alpha = - alpha_child.getF();
-        if(beta <= alpha)
-          return alpha_child;
-        parent.addChild(child);
-        if (alpha > parent.getChildren().peek().getF()){
-          child.setF(alpha);
-        }
-      }
-      parent.setF(parent.getChildren().peek().getF());
-      parent.setSquare(parent.getChildren().peek().getSquare());
-      return parent;
     }
 
-    @Override
-    public Square chooseSquare(Board board) {
-      Evaluator evaluate;
-      evaluate = new Evaluator(FunctionalUtils.coin_parity);
-      Square s = alphabeta(new Node(board), 3, Integer.MIN_VALUE, Integer.MAX_VALUE, evaluate).getSquare();
-      //System.out.println(board.getPlayerSquareCounts());
-      return s;
-        
+    Iterator<Node> children = child_list.iterator();
+    while (children.hasNext()) {
+      Node child = children.next();
+      Tuple alphaT = alphabeta(child, depth - 1, -beta, -alpha, evaluate);
+      alpha = - alphaT.score;
+      if(beta <= alpha) {
+        Tuple alpha2 = new Tuple(alphaT.node, alpha);
+        return alpha2;
+      }
+      if(alpha > bestResult){
+        //System.out.println("here");
+        bestResult = alpha;
+        best = child;
+      }
     }
-    
-    @Override
-    public void setChooseSquareTimeLimit(long time, TimeUnit unit) {
-    // by default, do nothing
-        time=1000;
-        unit=TimeUnit.MILLISECONDS;
-    }    
-   
+    //System.out.println(best.getSquare());
+    Tuple carson = new Tuple(best, bestResult);
+    return carson;
+  }
+  
+  @Override
+  public Square chooseSquare(Board board) {
+    Evaluator evaluate;
+    evaluate = new Evaluator(FunctionalUtils.coin_parity);
+    Square s = alphabeta(new Node(board), 4, Double.NEGATIVE_INFINITY, Double.POSITIVE_INFINITY, evaluate).node.getSquare();
+    return s;
+  }
+
+  @Override
+  public void setChooseSquareTimeLimit(long time, TimeUnit unit) {
+// by default, do nothing
+    time = 1000;
+    unit = TimeUnit.MILLISECONDS;
+  }
+
 }
 
-final class Node{
-    
-    private final Board node;
-    private int g;
-    private int h;
-    private int f;
-    private Square s;
-    private Player player;
-    private Player opponent;
-    private Queue<Node> children;
-    
-    public Node(Board node){
-        this.node = node;
-        this.children = new PriorityQueue(20, nodeComparator);
-        this.player = node.getCurrentPlayer();
-        this.opponent = node.getCurrentPlayer().opponent();
-        this.f = Integer.MIN_VALUE;
-    }
-    
-    public void play(Square a){
-      node.play(a);
-    }
-    
-    public Board getBoard(){
-        return node;
-    }
-    
-    public int getF(){
-        return f;
-    }
-    
-    public void setF(int f){
-        this.f = f;
-    }
-    
-    public int getH(){
-        return h;
-    }
-    
-    public void setSquare(Square s){
-        this.s = s;
-    }
-    
-    public Square getSquare(){
-        return s;
-    }
-    
-    public Player getPlayer(){
-      return player;
-    }
-    
-    public Player getOpponent(){
-      return opponent;
-    }
-    
-    public Queue<Node> getChildren(){
-        return children;
-    }
-    
-    public void addChild(Node child){
-        children.add(child);
-    }
-    public static Comparator<Node> nodeComparator = new Comparator<Node>(){
-        @Override
-        public int compare (Node n1, Node n2){
-            if(n1.getF() == n2.getF()){
-                return n1.getF();
-            } else{
-                return n1.getF() - n2.getF();
-            }
-        }
-    };
-}
+
 
 final class Memoizer<T, U> {
 
   private final Map<T, U> cache = new ConcurrentHashMap<>();
 
-  private Memoizer() {}
+  private Memoizer() {
+  }
 
   private Function<T, U> doMemoize(final Function<T, U> function) {
     return input -> cache.computeIfAbsent(input, function::apply);
@@ -201,4 +150,3 @@ final class Memoizer<T, U> {
     return new Memoizer<T, U>().doMemoize(function);
   }
 }
-
