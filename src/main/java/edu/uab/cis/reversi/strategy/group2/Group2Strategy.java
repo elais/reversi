a303806f1875a3d3ebd5f96053059dd6d32b1618
Elais Player <elais.player@gmail.com>
2015-04-03 19:08:37 -0500

    still not dominating random

--- src/main/java/edu/uab/cis/reversi/strategy/group2/Group2Strategy.java
+++ src/main/java/edu/uab/cis/reversi/strategy/group2/Group2Strategy.java
@@ -54,6 +54,15 @@ class FunctionalUtils {
   static final ToDoubleFunction<Node> valid_parity = (final Node node) -> {
     return coin_parity.applyAsDouble(node) + valid_moves.applyAsDouble(node);
   };
+  
+  static final ToDoubleFunction<Node> evaporation = (final Node node) -> {
+    if (node.getBoard().getPlayerSquareCounts().size() < 20)
+      return (double)node.getBoard().getPlayerSquareCounts().get(node.getOpponent())
+              - node.getBoard().getPlayerSquareCounts().get(node.getPlayer());
+    else 
+      return (double)node.getBoard().getPlayerSquareCounts().get(node.getPlayer())
+              - node.getBoard().getPlayerSquareCounts().get(node.getOpponent());
+  };
 }
 
 class Evaluator {
@@ -75,16 +84,37 @@ class Evaluator {
 
 public class Group2Strategy implements Strategy{
   //private long startTime;
-  private Map<Integer, Node> zobrist;
+  
+  //This is the alpha beta pruning algorithm
   private Tuple alphaBeta(Node node, int depth, double alpha, double beta, Evaluator evaluate) {
+    //Transposition talbe
+    double alphaOriginal = alpha;
     
-    if(System.currentTimeMillis() - startTime > TimeUnit.MILLISECONDS.toMillis(time - (time - 10L)))
+    //transposition table look up, node is the lookup key
+    TranspositionTable ttEntry = table.getOrDefault(node, null);
+    if (ttEntry != null && ttEntry.depth >= depth){
+      if(ttEntry.flag == ttEntry.flag.EXACT)
+        return new Tuple(ttEntry.node, ttEntry.value);
+      else if(ttEntry.flag == ttEntry.flag.LOWERBOUND)
+        alpha = Math.max(alpha, ttEntry.value);
+      else if(ttEntry.flag == ttEntry.flag.UPPERBOUND)
+        beta = Math.min(beta, ttEntry.value);
+      if(alpha >= beta)
+        return new Tuple(ttEntry.node, ttEntry.value);
+    }
+    
+        
+    
+    
+    if((System.currentTimeMillis() - startTime > TimeUnit.MILLISECONDS.toMillis(time - (time - 1L))) 
+            || node.getBoard().isComplete()){
       depth = 0;
+      if(node.getBoard().isComplete())
+        return new Tuple(node, evaluate.exec(node) * 100);
+    }
     // extract algorithm inputs from input tuple
     Node best = node;
-    double bestResult = Double.NEGATIVE_INFINITY; //placeholder for highest score so far
-    //if(System.nanoTime()  time - 100)
-    
+    double bestValue = Double.NEGATIVE_INFINITY; //placeholder for highest score so far
     // returns value of node in final depth
     if (depth == 0) {
       Tuple t = new Tuple(node, evaluate.exec(node));
@@ -111,34 +141,40 @@ public class Group2Strategy implements Strategy{
     while (children.hasNext()) {
       Node child = children.next();
       Tuple alphaT = alphaBeta(child, depth - 1, -beta, -alpha, evaluate);
-      alpha = - alphaT.score;
-      if(alpha > bestResult){
-        //System.out.println("here");
-        bestResult = alpha;
+      bestValue = Math.max(bestValue, -alphaT.score);
+      if(bestValue == -alphaT.score)
         best = child;
-      }
-      if(beta <= alpha){
-        return new Tuple(child, alpha);
-      }
+      alpha = Math.max(alpha, -alphaT.score);
+      if(alpha >= beta)
+        break;
     }
+    
+    // Transposition Table store; node is lookup key
+    TranspositionTable newEntry = new TranspositionTable(best, depth, bestValue);
+    if(bestValue <= alphaOriginal)
+      newEntry.flag = newEntry.flag.UPPERBOUND;
+    else if(bestValue >= beta)
+      newEntry.flag = newEntry.flag.LOWERBOUND;
+    else
+      newEntry.flag = newEntry.flag.EXACT;
+    table.put(node, newEntry);
     //System.out.println(best.getSquare());
-    Tuple t = new Tuple(best, bestResult);
+    Tuple t = new Tuple(best, bestValue);
     return t;
   }
   
-  
+  private Map<Node, TranspositionTable> table;
   @Override
   public Square chooseSquare(Board board) {
     
-    Evaluator evaluate = new Evaluator(FunctionalUtils.valid_parity); //choose an evaluation function
-    
-    //input tuple for algorithm, in a tuple for later hashing; values are immutable.
-    zobrist = new HashMap();
+    //The reversi overlord calls this function
+    Evaluator evaluate = new Evaluator(FunctionalUtils.valid_moves);
+    table = new ConcurrentHashMap<>(); // transpoisition table
     startTime = System.currentTimeMillis();
-    Square s = alphaBeta(new Node(board), 5, Double.NEGATIVE_INFINITY, Double.POSITIVE_INFINITY, evaluate)
-            .node.getSquare(); //gets square from algorithm
-    //System.out.println(s);
-    return s;
+    Tuple t; 
+    t = alphaBeta(new Node(board), 19, Double.NEGATIVE_INFINITY, Double.POSITIVE_INFINITY, evaluate);
+    //System.out.println(table.size());
+    return t.node.getSquare();
   }
   
   public long time;
@@ -150,10 +186,6 @@ public class Group2Strategy implements Strategy{
     this.time = t;
     this.unit = u;
   }
-  
-  private static final Map<FiveTuple,Tuple> memo = new HashMap<>();
-    
-  
   
 }
 
