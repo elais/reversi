83fb6c293c1cbe3ecc61620ed0f38a0b0b50385b
Elais Player <elais.player@gmail.com>
2015-04-06 08:30:42 -0500

    added a bunch of shit again

--- src/main/java/edu/uab/cis/reversi/strategy/group2/Group2Strategy.java
+++ src/main/java/edu/uab/cis/reversi/strategy/group2/Group2Strategy.java
@@ -39,11 +39,15 @@ public class Group2Strategy implements Strategy{
   
   //This is the alpha beta pruning algorithm
   public Leaf alphaBeta(Leaf leaf, int depth, double alpha, double beta, Evaluator evaluate) {
-    //Transposition table
-    double alphaOriginal = alpha;
-    List<Leaf> child_list = new ArrayList();
+    //these are checks put in to place to make sure we have not run out of time
+    //or that the search has now reached its max depth
+    //on reaching the time limit the search cuts off and calculates the nodes at
+    //the given depth
+ 
     
 
+    //Transposition table
+    double alphaOriginal = alpha;
     //transposition table look up, node is the lookup key
     //this speeds up search a little by eliminating the need to do needless
     //calculations on nodes already visited. 
@@ -59,28 +63,20 @@ public class Group2Strategy implements Strategy{
       if(alpha >= beta)
         return new Leaf(ttEntry.node, ttEntry.value, leaf.children);
     }
-    
-        
-    
-
-    //these are checks put in to place to make sure we have not run out of time
-    //or that the search has now reached its max depth
-    //on reaching the time limit the search cuts off and calculates the nodes at
-    //the given depth
-    if(leaf.node.getBoard().isComplete() 
-            || (System.nanoTime() - startTime) > TimeUnit.MILLISECONDS.toNanos(time - 100L)){ 
-        return new Leaf(leaf.node, evaluate.exec(leaf.node), child_list);
-      }
-    
-    if (depth == 0) {
-      return new Leaf(leaf.node, evaluate.exec(leaf.node), child_list);
+    List<Leaf> child_list = new ArrayList();
+   
+    if (leaf.node.getBoard().isComplete() || depth == 0) {
+      return new Leaf(leaf.node, evaluate.exec(leaf), child_list);
     }
     
     Leaf best = new Leaf(leaf.node, Double.NEGATIVE_INFINITY, child_list);
     double bestValue = Double.NEGATIVE_INFINITY; //placeholder for highest score so far
     // returns value of node in final depth
 
-    
+   if((System.nanoTime() - startTime) > TimeUnit.MILLISECONDS.toNanos(time - 100L)){
+        //System.out.println("here");
+        return null;
+     }    
     //here is where we define the node's children, first it checks to see if the node
     //has children, if not it creates them as needed (barring this is a game ending state etc
     if(leaf.children.isEmpty()){
@@ -100,9 +96,13 @@ public class Group2Strategy implements Strategy{
     } else {
       child_list = leaf.children;
     }
-    
-    
+    if ((System.nanoTime() - startTime) > TimeUnit.MILLISECONDS.toNanos(time - 100L)) {
+      //System.out.println("here");
+      return null;
+    }
     //truthfully, this is the meat of the algorithm
+    //importantly, after all of the child nodes are evaluated they
+    //are sorted 
     List<Leaf> tempChildList = new ArrayList(child_list.size());
     Iterator<Leaf> children = child_list.iterator();
     while (children.hasNext()) {
@@ -115,15 +115,22 @@ public class Group2Strategy implements Strategy{
         best = new Leaf(child.node, bestValue, child_list);
       }
       alpha = Math.max(alpha, -alphaT.score);
-      if(alpha >= beta)
+      if(alpha >= beta){
+        //child_list.sort(Comparator.comparing((Leaf e) -> e.score).reversed());
+        //Leaf newLeaf = new Leaf(alphaT.node, beta, alphaT.children);
+        //return newLeaf;
         break;
+      }
+
     }
-    if(!tempChildList.isEmpty()){
-      tempChildList.sort(Comparator.comparing(e -> e.score));
+    if ((System.nanoTime() - startTime) > TimeUnit.MILLISECONDS.toNanos(time - 100L)) {
+      //System.out.println("here");
+      return null;
+    }  
+    if (!tempChildList.isEmpty()) {
+      tempChildList.sort(Comparator.comparing((Leaf e) -> e.score).reversed());
       child_list = tempChildList;
     }
-
-    
     //Transposition Table store; node is lookup key
     TranspositionTable newEntry = new TranspositionTable(depth, bestValue, best.node);
     if(bestValue <= alphaOriginal)
@@ -162,24 +169,30 @@ public class Group2Strategy implements Strategy{
   private Map<Node, TranspositionTable> table;
   private final int infinity = Integer.MAX_VALUE;
   private int currentDepth = 0;
+  //private int maxDepth;
   @Override
   public Square chooseSquare(Board board){
     
     Evaluator evaluate;
-    evaluate = new Evaluator(Heuristics.mobility);
+    evaluate = new Evaluator(Heuristics.motherfucker);
     table = new HashMap<>(); // transposition table  
     startTime = System.nanoTime();
-    //int count = 0;
+    //maxDepth = 2;
+    Leaf pretender;
     Leaf root = new Leaf(new Node(board), Double.NEGATIVE_INFINITY, new ArrayList());
-    for(int i = 0; i <= 10; i++){
-      root = alphaBeta(root, i, Double.NEGATIVE_INFINITY, Double.POSITIVE_INFINITY, evaluate);
-      if(((System.nanoTime() - startTime) > TimeUnit.MILLISECONDS.toNanos(time - 100L)))
-        //System.out.println(i);
-        break;
+    pretender = root;
+    for(int maxDepth = 2; maxDepth < 6; maxDepth++) {
+      pretender = alphaBeta(root, maxDepth, Double.NEGATIVE_INFINITY, Double.POSITIVE_INFINITY, evaluate);
+      if(((System.nanoTime() - startTime) > TimeUnit.MILLISECONDS.toNanos(time - 100L))){
+        //System.out.println("here");
+        break; 
+      }
+      if(pretender != null)
+        root = pretender;
     }
-    //System.out.println(table.size());
-    //System.out.println(root.node.getSquare());
-    //System.out.println(currentDepth);
+    //System.out.println(root.children.get(0).score);
+    //System.out.println(root.children.get(1).score);
+    //System.out.println(root.children.get(2).score);
     return root.node.getSquare();
   }
 
@@ -193,20 +206,4 @@ public class Group2Strategy implements Strategy{
     this.unit = u;
   }
   
-}
-
-
-final class Memoizer<T, U> {
-
-  private final Map<T, U> cache = new ConcurrentHashMap<>();
-
-  private Memoizer() {}
-
-  private Function<T, U> doMemoize(final Function<T, U> function) {
-    return input -> cache.computeIfAbsent(input, function::apply);
-  }
-
-  public static <T, U> Function<T, U> memoize(final Function<T, U> function) {
-    return new Memoizer<T, U>().doMemoize(function);
-  }
 }
